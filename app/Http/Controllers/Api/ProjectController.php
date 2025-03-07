b99193358302c9fdde58822164b89c4b5cfd400b
Michael Belozerov <belozerov.mg@gmail.com>
2025-03-07 15:38:05 +0700

    added ExperienceController.php, ExperienceResource.php and modified api.php

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -15,4 +15,27 @@ class ProjectController extends Controller
     {
         return ProjectResource::collection(Project::all());
     }
+
+    public function show(Project $project)
+    {
+        return new ProjectResource($project);
+    }
+
+//    public function store(Request $request) {
+//
+//        // валидация
+//        $data = $request->validate([
+//            'title' => 'required|string',
+//            'description' => 'required|string',
+//            'link' => 'required|string',
+//            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+//        ]);
+//
+//        // сохранение image
+//        $data['image'] = $request->file('image')->store('images', 'public');
+//
+//        $project = Project::create($data);
+//
+//        return new ProjectResource($project);
+//    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\ExperienceController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/projects', ProjectController::class);
+Route::apiResource('/projects', ProjectController::class)
+    ->only(['index', 'show'])
+    ->parameter('projects', 'project');
+
+Route::apiResource('/experiences', ExperienceController::class)
+    ->only(['index', 'show'])
+    ->parameter('experiences', 'experience');
+
