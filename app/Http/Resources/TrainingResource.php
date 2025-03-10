b25759d611b488286527b4d8ec6c6d48e002713f
Michael Belozerov <belozerov.mg@gmail.com>
2025-03-10 11:13:04 +0700

    added UserController.php, UserResource.php and modified api.php

---
+++ app/Http/Resources/TrainingResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TrainingResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // будет приходить весь объект с возмож. преобр. данных
+        return [
+          'id' => $this->id,
+          'title' => $this->title,
+          'qualification' => $this->qualification,
+          'program_name' => $this->program_name,
+          'date_from' => Carbon::parse($this->date_from)->translatedFormat('F Y'),
+          'date_to' => Carbon::parse($this->date_to)->translatedFormat('F Y'),
+        ];
+    }
+}
